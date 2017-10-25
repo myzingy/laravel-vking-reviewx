d27f5e1c413500973b6ebd999fbc528c80951d34
wangfan <251941666@qq.com>
2017-10-25 18:00:13 +0800

    amarley points

--- config/review.php
+++ config/review.php
@@ -5,16 +5,16 @@ return [
         'domain'=>'http://laravel.vking',
         'brand'=>'Vking-Jeulia',
 
-        'api'=>'https://sandbox.jeulia.com/points/api/update',
-        'apiPublicKey'=>'sandbox888',
-        'apiSecretKey'=>'on6dxo7QDpZWM',
+        'api'=>'https://sandbox.amarley.com/points/api/update',
+        'apiPublicKey'=>'amarley888',
+        'apiSecretKey'=>'kadipRXR2iYHo',
     ],
     '49626cb84d647d4075d5ebe8da4b4b01'=>[
         'appkey'=>'912f5dc5b7fbce3076272a5cbe685591',
         'domain'=>'https://sandbox.amarley.com',
         'brand'=>'Sandbox-Amarley',
 
-        'api'=>'https://sandbox.amarley/points/api/update',
+        'api'=>'https://sandbox.amarley.com/points/api/update',
         'apiPublicKey'=>'amarley888',
         'apiSecretKey'=>'kadipRXR2iYHo',
     ],
